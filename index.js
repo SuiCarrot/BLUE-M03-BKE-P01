be33c5aa801120a2fef6fe33c09d0af4c4fc4b33
SuiCarrot <91708805+SuiCarrot@users.noreply.github.com>
2022-04-18 19:28:41 -0300

    style: index/controller/service Formatação dos arquivos utilizando prettier

--- index.js
+++ index.js
@@ -7,11 +7,8 @@ const route = require('./src/routes/tww.route.js');
 app.use(cors());
 app.use(express.json());
 
-
 app.use('/tww', route);
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
-
-
--- src/controllers/tww.controller.js
+++ src/controllers/tww.controller.js
@@ -1,31 +1,30 @@
 const lordsService = require('../services/tww.service.js');
 
+const findLordsController = (req, res) => {
+  res.send(lordsService.findLordsService());
+};
 
-const findLordsController = (req,res) => {
-    res.send(lordsService.findLordsService())
-}
+const findLordByIdController = (req, res) => {
+  res.send(lordsService.findLordByIdService(req.params.id));
+};
 
-const findLordByIdController = (req,res) =>{
-    res.send(lordsService.findLordByIdService(req.params.id))
-}
+const createLordController = (req, res) => {
+  res.send(lordsService.createLordService(req.body));
+};
 
-const createLordController = (req,res) => {
-    res.send(lordsService.createLordService(req.body));
-}
+const updateLordController = (req, res) => {
+  res.send(lordsService.updateLordService(+req.params.id, req.body));
+};
 
-const updateLordController = (req,res) =>{
-    res.send(lordsService.updateLordService(+req.params.id, req.body))
-}
-
-const deleteLordController = (req,res)=>{
-    lordsService.deleteLordService(req.params.id);
-    res.send({message:'Legendary Lord sucessfully deleted!'})
-}
+const deleteLordController = (req, res) => {
+  lordsService.deleteLordService(req.params.id);
+  res.send({ message: 'Legendary Lord sucessfully deleted!' });
+};
 
 module.exports = {
-    findLordsController,
-    findLordByIdController,
-    createLordController,
-    updateLordController,
-    deleteLordController,
-}
+  findLordsController,
+  findLordByIdController,
+  createLordController,
+  updateLordController,
+  deleteLordController,
+};
